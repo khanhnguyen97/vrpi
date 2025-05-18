c8b55736821491e42389ece1f6522f2893a3bb39
khanhnguyen97 <nguyenkhak97@gmail.com>
2025-05-19 04:42:45 +0700

    Update m.sh

--- m.sh
+++ m.sh
@@ -1,1 +1,1 @@
-nohup sh -c "while true; do (chmod +x main.sh && ./main.sh) || true; echo 'Retrying main.sh in 30s...'; sleep 30; done" > mainout.log 2>&1 &
+nohup sh -c "chmod +x main.sh && ./main.sh" > mainout.log 2>&1 &
