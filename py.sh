f68c07d38357f360bca329baebeb64feeced96fa
khanhnguyen97 <nguyenkhak97@gmail.com>
2025-05-19 04:41:24 +0700

    Update py.sh

--- py.sh
+++ py.sh
@@ -1,1 +1,1 @@
-nohup "while true; do (chmod +x pypya.sh && ./pypya.sh) || true; echo 'Retrying pypya.sh in 30s...'; sleep 30; done" > pyout.log 2>&1 &
+nohup sh -c "while true; do (chmod +x pypya.sh && ./pypya.sh) || true; echo 'Retrying pypya.sh in 30s...'; sleep 30; done" > pyout.log 2>&1 &
