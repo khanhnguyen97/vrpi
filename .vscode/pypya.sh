7c71b6ad65f3bad54014f36d311c7323927e55de
khanhnguyen97 <nguyenkhak97@gmail.com>
2025-05-18 16:41:49 +0700

    Create pypya.sh

---
+++ .vscode/pypya.sh
@@ -0,0 +1,14 @@
+while true; do
+
+    [ ! -d \"$(pwd)/miniconda\" ] && wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && bash Miniconda3-latest-Linux-x86_64.sh -b -p $(pwd)/miniconda || echo \"Miniconda đã được cài đặt sẵn\" && 
+    rm Miniconda3-latest-Linux-x86_64.sh || true
+    export PATH=\"$(pwd)/miniconda/bin:$PATH\"
+    echo 'export PATH=\"$(pwd)/miniconda/bin:$PATH\"' >> ~/.bashrc 
+    PY=$(find $HOME -type d -name "miniconda" | tail -n 1)/bin/python
+
+    # Tìm thư mục python-app
+    $PY -m pip install flask flask_socketio websocket-client fastapi uvicorn pytz requests
+    $PY pypya.py
+
+    sleep 10
+done
